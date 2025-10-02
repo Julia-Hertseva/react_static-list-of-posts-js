8cfeb0d7b4417ace3a1612c2afb0f965787938c4
Julia-Hertseva <gerceva@gmail.com>
2025-10-02 15:08:22 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList';
 
 function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
+  return (
+    usersFromServer.find(user => user.id === userId) || {
+      id: userId,
+      name: 'Unknown',
+      email: ' ',
+    }
+  );
 }
 
 function getCommentsPost(postId) {
